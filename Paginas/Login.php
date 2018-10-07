813d7cf9e81b174e329a639a61ebcce097f7889f
Wrcz <wrcz@hotmail.com>
2018-10-06 21:23:15 -0600

    .

--- Paginas/Login.php
+++ Paginas/Login.php
@@ -4,12 +4,12 @@
     //require_once("{$base_dir}Db{$ds}DbConnection.php");  //Error Fatal
 //echo phpinfo();
     $file = "{$base_dir}Db{$ds}DbConnection.php";
-     INclude_once($file);  //Advertencia
+     Include_once($file);  //Advertencia
 
     //$BASEURL ="/Segumg-App1/" ;
-    $BASEURL="/";
+    $BASEURL="../";
 
-Ñ  ?>
+ ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
